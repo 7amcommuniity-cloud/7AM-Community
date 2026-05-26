2f8cd5a56bdcba96bba7edd131a60a6477e8b288
7amcommuniity-cloud <7amcommuniity@gmail.com>
2026-05-27 01:41:09 +0530

    qr code replacement

--- src/App.tsx
+++ src/App.tsx
@@ -430,12 +430,12 @@ const SignupModal = ({ isOpen, onClose, selectedEvent }: { isOpen: boolean, onCl
                     <div className="absolute -inset-4 bg-brand-yellow/30 blur-2xl opacity-20 group-hover:opacity-50 transition duration-1000"></div>
                     <div className="relative bg-white p-3 md:p-4 rounded-xl shadow-[0_0_50px_rgba(255,255,0,0.2)] border-2 border-brand-yellow/30">
                       <img 
-                        src="public/qrr.jpeg" 
+                        src="/qrr.jpeg" 
                         alt="Payments QR" 
                         className="w-full h-full object-contain rounded-lg"
                         onError={(e) => {
                           const target = e.target as HTMLImageElement;
-                          target.src = "public/qrr.jpeg"; 
+                          target.src = "/qrr.jpeg"; 
                         }}
                       />
                     </div>
